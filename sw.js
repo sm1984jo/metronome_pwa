fb29f313915bc3ec61279e9ff932842a5d375ce5
sm1984jo <j.seongmin@gmail.com>
2025-10-08 04:53:07 +0000

    initial

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open('metronome-v1').then(cache => cache.addAll([
+    './', './index.html', './manifest.webmanifest', './sw.js',
+    './icons/icon-192.png','./icons/icon-512.png'
+  ])));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request)).catch(()=> new Response('',{status:504}))
+  );
+});
